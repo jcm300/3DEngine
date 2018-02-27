58bdcc035431458333f2b3e7e7c41d8712ca0d13
MQuaresma <miguelquaresma97@gmail.com>
2018-02-27 15:21:38 +0000

    Basic project structure defined

---
+++ src/Engine/engine.c
@@ -0,0 +1,13 @@
+/**
+ * engine.c
+ * Purpose: Reads an XML file specifying which models to render and the files in which this models have been specified. 
+ *
+ * @author João Ferreira 
+ * @author José Martins
+ * @author Miguel Quaresma
+ * @author Simão Barbosa
+ */
+
+int main(int argc, char *argv[]){
+
+}
---
+++ src/Generator/generator.c
@@ -0,0 +1,13 @@
+/**
+ * generator.c
+ * Purpose: Generates a config file with defining the given geometrical primitives, passed as parameter.
+ *
+ * @author João Ferreira 
+ * @author José Martins
+ * @author Miguel Quaresma
+ * @author Simão Barbosa
+ */
+
+int main(int argc, char *argv[]){
+
+}
